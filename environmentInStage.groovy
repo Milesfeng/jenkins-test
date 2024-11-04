f6f4e358e1a9614d8587fb2a5ddc45fb6a3e4f43
Milesfeng <milesfeng18@gmail.com>
2024-11-04 19:37:53 +0800

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -30,7 +30,9 @@ pipeline {
           sh 'echo "GITHUB_PR_TITLE: $GITHUB_PR_TITLE"'
           sh 'echo "GITHUB_PR_TRIGGER_SENDER_EMAIL: $GITHUB_PR_TRIGGER_SENDER_EMAIL"'
           sh 'echo "GITHUB_PR_COMMIT_AUTHOR_EMAIL: $GITHUB_PR_COMMIT_AUTHOR_EMAIL"'
-          sh 'echo "GITHUB_PR_HEAD_SHA: $GITHUB_PR_HEAD_SHA"'        
+          sh 'echo "GITHUB_PR_HEAD_SHA: $GITHUB_PR_HEAD_SHA"'
+          sh 'echo "CHANGE_ID: ${env.CHANGE_ID}"
+          sh 'echo "CHANGE_TITLE: ${env.CHANGE_TITLE}"      
       }
     }
     stage('Get Changes') {
