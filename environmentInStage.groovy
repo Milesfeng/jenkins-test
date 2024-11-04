fc2fad5023d3e581e34699c3e0a5abe441391997
Miles <milesfeng18@gmail.com>
2024-11-05 05:18:26 +0800

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -66,6 +66,9 @@ pipeline {
               echo "${currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause')}"
               echo "${currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').userName}"
               echo "${currentBuild.getBuildCauses()[0].shortDescription}"
+              cause = currentBuild.getBuildCauses()[0].shortDescription
+              echo $cause | awk '{print $NF}'
+
             }
         }
     }
