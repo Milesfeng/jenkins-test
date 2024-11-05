d9530a1ae3e5b2ec1a732a6e19623d1cfbe59676
Miles <milesfeng18@gmail.com>
2024-11-05 17:13:05 +0800

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -7,20 +7,20 @@ pipeline {
   }
 
   stages {
-    stage("local") {
-      environment {
-        // BAR will only be available in this stage
-        BAR = "STAGE"
-      }
-      steps {
-        sh 'echo "FOO is $FOO and BAR is $BAR"'
-      }
-    }
-    stage("global") {
-      steps {
-        sh 'echo "FOO is $FOO and BAR is $BAR"'
-      }
-    }
+    // stage("local") {
+    //   environment {
+    //     // BAR will only be available in this stage
+    //     BAR = "STAGE"
+    //   }
+    //   steps {
+    //     sh 'echo "FOO is $FOO and BAR is $BAR"'
+    //   }
+    // }
+    // stage("global") {
+    //   steps {
+    //     sh 'echo "FOO is $FOO and BAR is $BAR"'
+    //   }
+    // }
     stage("pr-test") {
       steps {
           sh 'echo "GIT_COMMIT: $GIT_COMMIT"'
